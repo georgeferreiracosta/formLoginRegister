db417013639688f05d9a20b83cf7c6ecb0cdbec3
mrgazo <mrgazo@kali.georgefctecdev>
2024-11-03 07:26:01 -0500

    o codigo comentado.

--- formulario/script.js
+++ formulario/script.js
@@ -1,12 +1,20 @@
+// Seleciona o elemento principal do container
 const container = document.querySelector('.container');
+
+// Seleciona o botão de registro
 const registerBtn = document.querySelector('.register-btn');
+
+// Seleciona o botão de login
 const loginBtn = document.querySelector('.login-btn');
 
+// Adiciona um evento de clique ao botão de registro
 registerBtn.addEventListener('click', () => {
+    // Adiciona a classe 'active' ao container, ativando o formulário de registro
     container.classList.add('active');
 });
 
+// Adiciona um evento de clique ao botão de login
 loginBtn.addEventListener('click', () => {
+    // Remove a classe 'active' do container, ativando o formulário de login
     container.classList.remove('active');
 });
-
